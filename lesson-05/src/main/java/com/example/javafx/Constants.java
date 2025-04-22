9c8786308efd7b9b3e6ce4291d895c8a267935cf
magichustler <magichustler01@gmail.com>
2025-04-22 15:42:44 -0400

    Add lesson-05

---
+++ lesson-05/src/main/java/com/example/javafx/Constants.java
@@ -0,0 +1,10 @@
+package com.example.javafx;
+
+public class Constants {
+    public static final int SCENE_WIDTH = 900;
+    public static final int SCENE_HEIGHT = 600;
+    public static final String FONT_1 = "DejaVu Serif";
+    public static final String FONT_2 = "FreeMono";
+    public static final String FONT_3 = "Ubuntu";
+    public static final String FONT_4 = "Ubuntu Thin";
+}
---
+++ lesson-05/src/main/java/com/example/javafx/Lesson5Application.java
@@ -0,0 +1,152 @@
+package com.example.javafx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.InnerShadow;
+import javafx.scene.effect.Reflection;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
+
+import static com.example.javafx.Constants.*;
+
+public class Lesson5Application extends Application {
+
+    private BorderPane principalPanel;
+    private BorderPane centralPanel;
+    private AnchorPane drawingPanel;
+    private AnchorPane textPanel;
+    private ComboBox comboBox;
+
+    // Visual components for textPanel
+    private Button drawButton;
+    private Text text;
+    private TextArea textArea;
+
+    private Label labelFont;
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        createGUI(stage);
+        setActions();
+    }
+
+
+    private void createGUI(Stage primaryStage) {
+        principalPanel = new BorderPane();
+
+        centralPanel = new BorderPane();
+        principalPanel.setCenter(centralPanel);
+
+        createDrawingPanel();
+        centralPanel.setCenter(drawingPanel);
+
+        createTextPanel();
+        centralPanel.setTop(textPanel);
+
+        Scene scene = new Scene(principalPanel, SCENE_WIDTH, SCENE_HEIGHT);
+
+        primaryStage.setTitle("Aplicatie JAVA FX 04");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void setActions() {
+        drawButton.setOnAction(event -> {
+            changeText();
+        });
+    }
+
+    private void changeText() {
+        text.setFont(Font.font(comboBox.getValue().toString()));
+        text.setText(textArea.getText());
+    }
+
+    private void createDrawingPanel() {
+        drawingPanel = new AnchorPane();
+        drawingPanel.setStyle("-fx-background-color: #ffffff;");
+
+        text = createText("Java is super", 100, 100, 50, Color.CORAL);
+
+        drawingPanel.getChildren().add(text);
+
+    }
+
+    private void createTextPanel() {
+        textPanel = new AnchorPane();
+
+        textArea = createTextArea(30, 70, 550, 70);
+        drawButton = createButton("Draw the text", 600, 80);
+
+        textPanel.getChildren().addAll(textArea, drawButton);
+
+        labelFont = createLabel("Font: ", 30, 10);
+        textPanel.getChildren().add(labelFont);
+
+
+        comboBox = createComboBox(30 ,30);
+        textPanel.getChildren().add(comboBox);
+
+
+
+    }
+
+    private Text createText(String title, int x, int y, int size, Color color) {
+        Text createdText = new Text(x, y, title);
+        createdText.setFont(Font.font(size));
+        createdText.setFill(color);
+        return  createdText;
+    }
+
+    private TextArea createTextArea(int x, int y, int width, int height) {
+        TextArea textArea = new TextArea();
+        textArea.setLayoutX(x);
+        textArea.setLayoutY(y);
+        textArea.setMinWidth(width);
+        textArea.setMaxWidth(width);
+        textArea.setMinHeight(height);
+        textArea.setMaxHeight(height);
+        return textArea;
+    }
+
+    private Button createButton(String title, int x, int y) {
+        Button button = new Button();
+        button.setText(title);
+        button.setLayoutX(x);
+        button.setLayoutY(y);
+        button.setMinHeight(30);
+        button.setTextAlignment(TextAlignment.CENTER);
+        return button;
+    }
+
+    private ComboBox createComboBox(int x, int y) {
+        ComboBox comboBoxToCreate = new ComboBox();
+        comboBoxToCreate.getItems().add(FONT_1);
+        comboBoxToCreate.getItems().add(FONT_2);
+        comboBoxToCreate.getItems().add(FONT_3);
+        comboBoxToCreate.getItems().add(FONT_4);
+        comboBoxToCreate.setLayoutX(x);
+        comboBoxToCreate.setLayoutY(y);
+
+        return comboBoxToCreate;
+    }
+
+    private Label createLabel(String text, int x, int y) {
+        Label label = new Label(text);
+        label.setLayoutX(x);
+        label.setLayoutY(y);
+        return label;
+    }
+
+
+}
